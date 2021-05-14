3da64783e5535cb0b4ebc1c676fd5e2f2b006abc
Harshit Kumar Gupta <harshitkg1234@gmail.com>
2021-05-14 23:27:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function sendMoney(){
+    var enterName = document.getElementById("enterName").value;
+    var enterAmount = parseInt(document.getElementById("enterAmount").value);
+ var enterSName = document.getElementById("enterSName").value;
+  var findSenderBankAccount = enterSName + "BankBalance";
+    var enterSAmount =parseInt(document.getElementById(findSenderBankAccount).innerHTML);
+    if (enterAmount > enterSAmount) {
+       alert("Your Balance is Insufficient for Successful Transaction")
+    }
+ else {
+       var findUserBankAccount = enterName + "BankBalance";
+      
+       var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
+       var myAccountBalance = parseInt(document.getElementById(findSenderBankAccount).innerHTML) - enterAmount;
+       document.getElementById(findSenderBankAccount).innerHTML = myAccountBalance;
+       document.getElementById(findUserBankAccount).innerHTML = finalAmount;
+       alert(`Congratulations, The transaction is Successful !!  
+       $${enterAmount} is sent to ${enterName} with email-Id ${enterName}@email.com.`)
+ 
+       // transaction history 
+       var createPTag = document.createElement("li");
+       var textNode = document.createTextNode(`${enterName}@email.com received Rs ${enterAmount} from ${enterSName}@email.com on ${Date()}.`);
+       createPTag.appendChild(textNode);
+       var element = document.getElementById("transaction-history-body");
+       element.insertBefore(createPTag, element.firstChild);
+    }
+ }
